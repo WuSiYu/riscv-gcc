cf4feb7e74a99a15e0e4fd1b0a209d5d42905d33
linsinan1995 <47880367+linsinan1995@users.noreply.github.com>
2021-02-04 03:47:54 +0200

    Fix riscv_expand_block_move

--- gcc/config/riscv/riscv.c
+++ gcc/config/riscv/riscv.c
@@ -3491,7 +3491,7 @@ riscv_block_move_loop (rtx dest, rtx src, unsigned HOST_WIDE_INT length,
 bool
 riscv_expand_block_move (rtx dest, rtx src, rtx length)
 {
-  if (CONST_INT_P (length))
+  if (CONST_INT_P (length) && INTVAL (length) >= 0)
     {
       unsigned HOST_WIDE_INT hwi_length = UINTVAL (length);
       unsigned HOST_WIDE_INT factor, align;
